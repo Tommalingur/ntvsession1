3e42be2ba880b6bc768114dff995d22b0e3f3465
Tómas <tomashg1985@gmail.com>
2021-10-13 14:20:50 +0000

    project complete

---
+++ bin/timaverkefni1.dart
@@ -0,0 +1,20 @@
+
+  void main(List<String> arguments) {
+
+  // print is used to print out the code inside the {} in witch this code is between.
+  // always use single quote unless you have to use a apostrophe inside the quotes.
+  // Example: 'Hello world', "Tom's world".
+   print('Hello world!');
+
+   print(2 + 3);
+   print(2 / 3);
+   print(2 * 3);
+  // Notice the calculation below, the first one adds 2 to 3 * 3, but the second adds 2 to 3, and then multiplies by 3.
+   print(2 + 3 * 3);
+   print((2 + 3) * 3);
+    print(16 / 3);
+  // the % is division with remainders, also called Euclidean division.
+   print(16 % 3);
+
+}
+
